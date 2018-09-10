90776c82d2945a5396d4dc7946344243c67228dd
Marc Eastman <eastman@umd.edu>
2018-09-09 22:22:36 -0400

    fratal changes on click

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+*still updating but it works*
+
 Trying it yourself
 ==========
 
@@ -5,30 +7,38 @@ Trying it yourself
 git clone (project url)
 start ./fractals/first-try/index.html
 ```
-(or open index.html another way)
+(or open index.html *in a browser* another way)
 
 Overview
 ==========
 
 A fractal is a shape that is the same when you zoom in on a specific part. In the
-below example from
-![diamond-with-graphics.js](./first-try/diamond-with-graphics.js),
-notice how the 2 sides are identical to the whole:
-![hourglass-fractal](https://media.giphy.com/media/BZiYd6miQbM4oCo0Zx/giphy.gif)
+hourglass part below, notice how the 2 sides are identical to the whole:
+![fractal-demonstration](https://media.giphy.com/media/xBoMMLbTUP4i0EL9TK/giphy.gif)
 
-
-*Note*: It's not animated, you control it with the mousewheel.
-
-Getting Around
-==============
-
-To switch which fractal you are rendering, change the code in
-![index.html](./first-try/index.html). Like for example, you could change
-`<script src="./hexagon.js"></script>` to `<script src="./sketch.js"></script>` and
-that's all you need to do.
 ![sketch.js](./first-try/sketch.js) was my first try and is very slow, but you
 can control the color with the slider on the left and the fractal depth with the
-slider on the right.
+slider on the right. If you want to see this, change the script that is being
+rendered in ![index.html](./first-try/index.html)
+
+Playing
+==============
+
+To switch which fractal you are rendering, click the screen.
+*Note*: It's not animated, you control it with the mousewheel.
+
+
+Future Plans
+============
+
+- syncing up to music
+- playing with sound
+- circle fractals
+- increase speed
 
 Challenges
 ========
+
+1. I was having trouble going back in depth of the fractal. **Solved**: Everytime
+the depth of the fractal changes, the picture is redrawn into a graphics buffer
+and re-rendered.
--- first-try/hexagon.js
+++ first-try/hexagon.js
@@ -1,26 +1,53 @@
 var pg;
+var dg;
 var depth = 0;
 var bend1 = 0.5;
 var bend2 = 0.5;
+var currShape = "hexagon";
 
 var setup = () => {
   createCanvas(window.innerWidth-20, window.innerHeight-20);
   background(10);
-  stroke(10);
-
-  strokeWeight(1.1);
-  frameRate(10);
   noLoop();
   pg = createGraphics(width, height);
-  // pg.fill(100);
+  dg = createGraphics(width, height);
 }
 
 var draw = () => {
+  console.log(currShape);
   if(depth <= 0)
     return;
   pg.background(10);
-  drawHex(height/6, width/2, height/2, depth);
-  image(pg, 0, 0);
+  dg.background(10);
+  if(currShape == "hexagon"){
+    drawHex(height/6, width/2, height/2, depth);
+    image(pg, 0, 0);
+  }
+  else if(currShape == "diamond"){
+    //bottom
+    drawTriangle(width/2, height, height/4, height/4, -1, depth);
+    //top
+    drawTriangle(width/2, 0, height/4, height/4, 1, depth);
+    image(dg, 0, 0);
+  }
+}
+
+//draws one triangle
+drawTriangle = (x, y, xdist, ydist, mult, depth) => {
+  if(depth <= 0 || xdist < 1)
+    return;
+  dg.strokeWeight(2);
+  dg.stroke(noise(x)*255, noise(y+10000)*255, noise(5*x+5*y+20000)*255);
+  dg.line(x,y,x-xdist, y+ydist*mult);
+  dg.line(x,y,x+xdist, y+ydist*mult);
+  //left side
+  drawTriangle(x-xdist, y+ydist*mult, xdist*0.5, ydist*0.5, mult, depth-1);
+  // right side
+  drawTriangle(x+xdist, y+ydist*mult, xdist*0.5, ydist*0.5, mult, depth -1);
+  // outmost inner
+  drawTriangle(x, y, xdist*bend1, ydist*0.5, mult, depth -1);
+  // inner inner, starting from middle. uncomment this for grid fractal
+  // drawTriangle(x, y+2*ydist*mult, xdist*bend2, ydist*0.5, mult, depth -1);
 }
 
 //draws one hexagon
@@ -41,8 +68,6 @@ var drawHex = (curHeight, centerx, centery, depth) => {
   pg.vertex(centerx + (curHeight/sqrt(3)), centery - curHeight);
   pg.vertex(centerx - (curHeight/sqrt(3)), centery - curHeight);
   pg.endShape(CLOSE);
-
-
   //left
   drawHex(curHeight/2, centerx - 3*(curHeight/sqrt(3)), centery, depth-1);
   //bottom (starting with left)
@@ -57,9 +82,18 @@ var drawHex = (curHeight, centerx, centery, depth) => {
 
 var mouseWheel = (event) =>{
   (event.delta <= 0 ? depth ++ : depth --);
-  if(depth < -1)
-    depth = -1;
+  if(depth < 1)
+    depth = 1;
   if(depth > 8)
     depth = 8;
+  redraw();
+}
+
+var mouseClicked = () => {
+  depth = 1;
+  switch(currShape){
+    case "hexagon": currShape = "diamond"; break;
+    case "diamond": currShape = "hexagon"; break;
+  }
   redraw();
 }
